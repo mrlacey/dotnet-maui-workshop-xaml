36d408b6ee6ced0e4c9db25b2d2c6493c8e60f12
Matt Lacey <matt@mrlacey.co.uk>
2024-03-05 20:33:28 +0000

    initial code setup

---
+++ Part 5 - Custom Types/Finish/MonkeyFinder/Extensions/GetRadius.cs
@@ -0,0 +1,23 @@
+﻿namespace MonkeyFinder.Extensions;
+
+public class GetRadius : BindableObject, IMarkupExtension<double>
+{
+    public double Diameter
+    {
+        get => (double)GetValue(DiameterProperty);
+        set => SetValue(DiameterProperty, value);
+    }
+
+    public static readonly BindableProperty DiameterProperty =
+        BindableProperty.Create(nameof(Diameter), typeof(double), typeof(GetRadius), defaultValue: 0.0);
+
+    public double ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Diameter / 2;
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return (this as IMarkupExtension<double>).ProvideValue(serviceProvider);
+    }
+}
